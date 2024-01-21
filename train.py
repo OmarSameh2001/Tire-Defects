67f72d3853064cf2bc5495cf2a10ab39c57e10ce
Omar Sameh <120422966+OmarSameh2001@users.noreply.github.com>
2024-01-21 15:47:55 +0200

    Create train.py

---
+++ train.py
@@ -0,0 +1,12 @@
+#install ultralytics in terminal (!pip install ultralytics)
+
+from ultralytics import YOLO
+
+
+# Load a model
+model = YOLO("yolov8x-cls.pt")  # load a pretained model
+
+# Use the model (Change question mark(?...?))
+results = model.train(data=?path/to/dataest?, epochs=?number of epoces?, imgsz=640)  # train the model
+
+#Classification
