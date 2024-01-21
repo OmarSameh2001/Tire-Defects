c29b6c580851a0c7aab60d2cf963e958becc3ef9
Omar Sameh <120422966+OmarSameh2001@users.noreply.github.com>
2024-01-21 15:49:15 +0200

    Update train.py

--- train.py
+++ train.py
@@ -8,4 +8,4 @@ model = YOLO("yolov8x-cls.pt")  # load a pretained model
 # Use the model (Change question mark(?...?))
 results = model.train(data=?path/to/dataest?, epochs=?number of epoces?, imgsz=640)  # train the model
 
-#Classification
+#*This model is for Classification*
